debf3695b63709402b7ceafadf9d586fd0c4d042
MitkoNed21 <mitkoned21@gmail.com>
2024-01-14 16:18:21 +0200

    ADD: resources and collections for models

---
+++ app/Http/Resources/HolidayResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HolidayResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "location" => $this->location,
+            "title" => $this->title,
+            "startDate" => $this->start_date,
+            "duration" => $this->duration,
+            "price" => $this->price,
+            "freeSlots" => $this->free_slots,
+        ];
+    }
+}
